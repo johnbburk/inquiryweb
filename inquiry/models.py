bbe64651962133d5f9716f37cf37c8a0d5ef5e65
John Burk <john.burk98@gmail.com>
2012-01-23 20:24:47 -0500

    first commit

---
+++ inquiry/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+#this is a comment
+class Author(models.Model):
+    firstName = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=20)
+    
+class Question (models.Model):
+    question = models.CharField(max_length=300)
+    pub_date = models.DateTimeField('datepublished')
+    author = models.ForeignKey(Author)
+    
+# Create your models here.
