0850729a3196909a6b791f8703d28b52e3eff915
John Burk <john.burk98@gmail.com>
2012-01-29 22:09:18 -0500

    changed models.py to support Event model
    since all of the classes share data, the Event model provides a good
    way to make them a single class.
    
    got the idea from http://earthandnot.wordpress.com/

--- inquiry/models.py
+++ inquiry/models.py
@@ -1,29 +1,30 @@
 from django.db import models
 
 #this is a comment
+
+# use http://code.google.com/p/django-tagging/ or something else for tagging?
 class Author(models.Model):
-    firstName = models.CharField(max_length=20)
-    lastName = models.CharField(max_length=20)
-    
-class Question (models.Model):
-    question = models.CharField(max_length=300)
-    pub_date = models.DateTimeField('datepublished')
-    author = models.ForeignKey(Author)
-    
-class Prediction (models.Model):
-    prediction = models.CharField(max_length=300)
-    pub_date = models.DateTimeField('datepublished')
-    author = models.ForeignKey(Author)
-    
-class Observation (models.Model):
-    observation = models.CharField(max_length=300)
-    pub_date = models.DateTimeField('datepublished')
-    author = models.ForeignKey(Author)
-    
-class Answer (models.Model):
-    Answer = models.CharField(max_length=300)
-    pub_date = models.DateTimeField('datepublished')
-    author = models.ForeignKey(Author)
-               
+firstName = models.CharField(max_length=25)
+lastName = models.CharField(max_length=25)
+# email address?
+# adorable picture?
+def __unicode__(self):
+return self.firstName + u” ” + self.lastName
+EVENT_CHOICES = (
+(‘Q’, ‘Question’),
+(‘P’, ‘Prediction’),
+(‘O’, ‘Observation’),
+(‘E’, ‘Explanation’),
+(‘M’, ‘Model’),
+)
+class Event (models.Model):
+pub_date = models.DateTimeField(‘datepublished’)
+author = models.ForeignKey(Author)
+referants = models.ManyToMany(Event)
+event_type = models.CharField(max_length=1, choices=EVENT_CHOICES)
+description = models.CharField(max_length=300)
+tag = models.ManyToManyField(Tag) # FIXME
+def __unicode__(self):
+return self.event_type + u”. ” + self.description
     
 # Create your models here.
