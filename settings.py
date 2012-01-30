66edf7d07075a7161cc9d2d6a1e2f3bd855fd5a1
John Burk <john.burk98@gmail.com>
2012-01-30 09:16:06 -0500

    added gitignore

--- settings.py
+++ settings.py
@@ -11,9 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
+        #'ENGINE':'mysql'
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'inquiryweb',                      # Or path to database file if using sqlite3.
+        #'USER': 'burkphys_django'
         'USER': 'django',                      # Not used with sqlite3.
+        #'PASSWORD':'',
         'PASSWORD': 'djangotest',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
