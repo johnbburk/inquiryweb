0ad10a2333d59caed91ab3fb4ea7eff9aca9a132
John Burk <john.burk98@gmail.com>
2012-01-29 22:14:47 -0500

    fixed indentation

--- inquiry/models.py
+++ inquiry/models.py
@@ -4,12 +4,13 @@ from django.db import models
 
 # use http://code.google.com/p/django-tagging/ or something else for tagging?
 class Author(models.Model):
-firstName = models.CharField(max_length=25)
-lastName = models.CharField(max_length=25)
-# email address?
-# adorable picture?
-def __unicode__(self):
-return self.firstName + u” ” + self.lastName
+    firstName = models.CharField(max_length=25)
+    lastName = models.CharField(max_length=25)
+    # email address?
+    # adorable picture?
+    def __unicode__(self):
+        return self.firstName + u” ” + self.lastName
+
 EVENT_CHOICES = (
 (‘Q’, ‘Question’),
 (‘P’, ‘Prediction’),
@@ -17,14 +18,15 @@ EVENT_CHOICES = (
 (‘E’, ‘Explanation’),
 (‘M’, ‘Model’),
 )
+
 class Event (models.Model):
-pub_date = models.DateTimeField(‘datepublished’)
-author = models.ForeignKey(Author)
-referants = models.ManyToMany(Event)
-event_type = models.CharField(max_length=1, choices=EVENT_CHOICES)
-description = models.CharField(max_length=300)
-tag = models.ManyToManyField(Tag) # FIXME
-def __unicode__(self):
-return self.event_type + u”. ” + self.description
+    pub_date = models.DateTimeField(‘datepublished’)
+    author = models.ForeignKey(Author)
+    referants = models.ManyToMany(Event)
+    event_type = models.CharField(max_length=1, choices=EVENT_CHOICES)
+    description = models.CharField(max_length=300)
+    tag = models.ManyToManyField(Tag) # FIXME
+    def __unicode__(self):
+        return self.event_type + u”. ” + self.description
     
 # Create your models here.
