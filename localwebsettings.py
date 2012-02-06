d202ae9290fad3ce8aa962f4de757182f9344e74
John Burk <john.burk98@gmail.com>
2012-02-06 22:13:05 +0000

    modified settings.py so that servercode is factored out into separate file.

---
+++ localwebsettings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE':'django.db.backends.mysql',
+        'NAME': 'occam98_inqweb',
+        'USER': 'occam98_inqweb',
+        'PASSWORD':'068e61d4',
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
--- settings.py
+++ settings.py
@@ -9,17 +9,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE':'django.db.backends.mysql',
-        'NAME': 'occam98_inqweb',
-        'USER': 'occam98_inqweb',
-        'PASSWORD':'068e61d4',
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
+from localwebsettings import *
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
