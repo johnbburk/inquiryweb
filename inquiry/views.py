be7ee7961cd43e325d1610b6b6d7767417750314
John Burk <john.burk98@gmail.com>
2012-02-08 02:54:19 +0000

    added basic index view and template to display list of questions.

--- inquiry/views.py
+++ inquiry/views.py
@@ -1,3 +1,11 @@
+from django.template import Context, loader
 from django.http import HttpResponse
+from inquiry.models import Event
+
 def index(request):
-    return HttpResponse("This is the default page for inquiryweb")
+    latest_question_list = Event.objects.all().order_by('mod_datetime')
+    t = loader.get_template('index.html')
+    c = Context({
+        'latest_question_list':latest_question_list,
+    })
+    return HttpResponse(t.render(c))
