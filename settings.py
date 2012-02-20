18b97b9d2e1b297096be3fde6f375f2b044e28c5
John Burk <john.burk98@gmail.com>
2012-02-20 03:25:46 +0000

    working program on server, before merging in changes.

--- settings.py
+++ settings.py
@@ -37,22 +37,22 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/home/occam98/webapps/django/inquiryweb/static/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/static/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/occam98/webapps/django/inquiryweb'
+STATIC_ROOT = '/home/occam98/webapps/django/inquiryweb/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'http://occam98.webfactional.com/static'
+STATIC_URL = 'http://occam98.webfactional.com/static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
@@ -60,7 +60,7 @@ STATIC_URL = 'http://occam98.webfactional.com/static'
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
-STATICFILES_DIRS = ( "/home/occam98/webapps/django/inquiryweb/static",
+STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
